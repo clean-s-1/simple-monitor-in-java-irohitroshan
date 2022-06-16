3eebb24e45e9b4c88ee0b05d64992c584a0ebf13
Rohit Roshan <rohit.roshan@siemens.com>
2022-06-16 22:19:51 +0530

    Reduced the cyclic complexity through enum

--- vitals/BatteryManagementSystem.java
+++ vitals/BatteryManagementSystem.java
@@ -1,45 +1,16 @@
 package vitals;
 
 public class BatteryManagementSystem {
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate, String batteryHealthFactor) {
-        boolean batteryState = true;
-        switch (batteryHealthFactor) {
-            case "temp":
-                batteryState = isTempOutOfRange(temperature);
-                break;
-            case "soc":
-                batteryState = isChargeStateOutOfRange(soc);
-                break;
-            case "rate":
-                batteryState = isChargeRateOutOfRange(chargeRate);
-                break;
-        }
-        return batteryState;
-    }
-
-    private static boolean isChargeRateOutOfRange(float chargeRate) {
-        System.out.println("Charge Rate is out of range!");
-        return !(chargeRate > 0.8);
-    }
-
-    private static boolean isChargeStateOutOfRange(float soc) {
-        System.out.println("State of Charge is out of range!");
-        return !(soc < 20 || soc > 80);
-    }
-
-    private static boolean isTempOutOfRange(float temperature) {
-        System.out.println("Temperature is out of range!");
-        return !(temperature < 0 || temperature > 45);
+    static boolean batteryIsOk(float temperature, float soc, float chargeRate, BatteryHealth batteryHealth) {
+        return batteryHealth.isOutOfRange(temperature, soc, chargeRate);
     }
 
     public static void main(String[] args) {
-        assert (batteryIsOk(25, 70, 0.7f, "temp") == true);
-        assert (batteryIsOk(50, 85, 0.0f, "temp") == false);
-        assert (batteryIsOk(25, 70, 0.7f, "soc") == true);
-        assert (batteryIsOk(25, 85, 0.0f, "soc") == false);
-        assert (batteryIsOk(25, 70, 0.9f, "rate") == false);
+        assert (batteryIsOk(25, 70, 0.7f, BatteryHealthFactor.valueOf("TEMPERATURE")) == true);
+        assert (batteryIsOk(50, 85, 0.0f, BatteryHealthFactor.valueOf("TEMPERATURE")) == false);
+        assert (batteryIsOk(25, 70, 0.7f, BatteryHealthFactor.valueOf("CHARGE_STATE")) == true);
+        assert (batteryIsOk(25, 85, 0.0f, BatteryHealthFactor.valueOf("CHARGE_STATE")) == false);
+        assert (batteryIsOk(25, 70, 0.9f, BatteryHealthFactor.valueOf("CHARGE_RATE")) == false);
         System.out.println("Battery Condition tested");
-
-
     }
 }
